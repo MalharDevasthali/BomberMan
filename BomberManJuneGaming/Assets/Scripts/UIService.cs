0284f16ba36043d4e6039b995d8b5d3021cf4d96
Malhar Devasthali <malhar@deftouch.co>
2020-10-14 18:00:08 +0530

    #Added - Basic Setup for First Scene

---
+++ BomberManJuneGaming/Assets/Scripts/UIService.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class UIService : MonoBehaviour
+{
+    public static UIService instance;
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+
+    private void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void StartTheGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
